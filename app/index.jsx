464d881b903551980cb236b971dd9d967a70fa92
Chise Tuna <cisefm123@gmail.com>
2024-06-22 16:41:21 +0100

    Changed the header logo.

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View, Image } from 'react-native';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
+
 
 export default function App() {
   return (
-    <SafeAreaView classname="bg-primary h-full">
+    <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%'}}>
         <View className="w-full justify-center items-center h-full px-4">
 
+          <Image
+            source={images.header}
+            className="max-w-[100px] w-full h-[100px]"
+            resizeMode="contain"
+          />
+
+          <Text className="text-center text-lg font-bold p-4 text-white">
+            Race to silence alarms
+          </Text>
+          <Text className="text-center text-lg font-bold p-4 text-white">
+            Survive hilarious wake-up calls from friends!
+          </Text>
+          
+          <Image
+            source={images.landingscreen}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+            
         </View>
       </ScrollView>
     </SafeAreaView>
